7d51f515aed3326abcab43dfe671726a663ea6a6
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2024-08-05 09:58:44 +0200

    vercel

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ app.get('/', (_, res) => {
     })
 })
 
-// app.use("/api", root)
+app.use("/api", root)
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`SERVER IS WORKED ON PORT ${process.env.PORT}`)
